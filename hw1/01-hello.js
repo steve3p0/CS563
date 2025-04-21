1a2b8440a2decc834cd52c191a257bc70396499f
SteveBraich <sbraich@pgls.com>
2025-04-21 16:32:00 -0700

    Complete Homework 1: Setup and Semantics

--- hw1/01-hello.js
+++ hw1/01-hello.js
@@ -1,5 +1,5 @@
 window.addEventListener('load', loadPage);
 
 function loadPage(event) {
-  console.log('The page has now loaded.');
+  console.log('My name is Steve Braich and I’m a graduate student in CS 563. I’m passionate about AI, full-stack development, and creating better user experiences. This quarter, I\'m excited to deepen my web development skills and build clean, accessible web applications.');
 }
